4a4434a0c261842606dc8da3ebb674e75368b96e
kanik155 <takekanik155@gmail.com>
2023-04-27 19:50:34 +0900

    Change Newtonsoft.Json to Unity.Plastic.Newtonsoft.Json.

--- Assets/Plugins/Witchpot/Packages/StableDiffusion/Runtime/Scripts/StableDiffusionWebUIClient.cs
+++ Assets/Plugins/Witchpot/Packages/StableDiffusion/Runtime/Scripts/StableDiffusionWebUIClient.cs
@@ -1,8 +1,8 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.Plastic.Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Networking;
 
