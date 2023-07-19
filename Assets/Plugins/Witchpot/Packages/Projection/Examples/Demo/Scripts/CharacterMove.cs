8fc776771c85f33a149067e5cd098ff3361b8406
nakashun-t <nakashun.t@gmail.com>
2023-07-19 21:31:41 +0900

    fix materials

--- Assets/Plugins/Witchpot/Packages/Projection/Examples/Demo/Scripts/CharacterMove.cs
+++ Assets/Plugins/Witchpot/Packages/Projection/Examples/Demo/Scripts/CharacterMove.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -11,8 +11,8 @@ public class CharacterMove : MonoBehaviour
     [SerializeField]
     private Camera m_Camera;
 
-    [SerializeField]
-    private Animator m_animator;
+    //[SerializeField]
+    //private Animator m_animator;
 
 
     private void Update()
@@ -25,17 +25,15 @@ public class CharacterMove : MonoBehaviour
             {
                 m_agent.destination = hit.point;
             }
-
         }
 
-
-        if (m_agent.remainingDistance < 1f)
-        {
-            m_animator.SetFloat("Speed", 0f);
-        }
-        else
-        {
-            m_animator.SetFloat("Speed", m_agent.desiredVelocity.magnitude);
-        }
+        //if (m_agent.remainingDistance < 1f)
+        //{
+        //    m_animator.SetFloat("Speed", 0f);
+        //}
+        //else
+        //{
+        //    m_animator.SetFloat("Speed", m_agent.desiredVelocity.magnitude);
+        //}
     }
 }
